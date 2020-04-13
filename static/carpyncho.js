87d40fc937ea0b0d09c1ab2f0fc5701e095346c6
Juan B Cabral <jbc.develop@gmail.com>
2020-04-13 19:18:25 -0300

    mostly done

--- static/carpyncho.js
+++ static/carpyncho.js
@@ -20,13 +20,44 @@ $.get({
 });
 
 
+var showDownloadModal = function(evt){
+    alert("foo");
+}
+
+
+var $downloadModal = $("#downloadModal");
+var $downloadModalLinks = $downloadModal.find("#downloadModalLinks");
 
 $.getJSON({
     url: "published.json",
     cache: false
 }).then(json => {
     $.each(json, function(tile, tdata){
-        $('td#tile-' + tile).addClass("table-primary tile-active");
+        var $tiletd = $('td#tile-' + tile);
+
+        $tiletd.addClass("table-primary tile-active");
+
+        $tiletd.click(function(evt){
+            $downloadModal.find("#downloadModalTileNameTitle").text(tile);
+            $downloadModalLinks.empty();
+
+            $.each(tdata, function(lname, ldata){
+               var links = " ".concat(
+                    '<li class="list-group-item d-flex justify-content-between align-items-center">',
+                    `<a href="${ldata['link']}" class="">`,
+                    `<i class="fa fa-download"></i> ${ldata["hname"]}`,
+                    `<small> <code>(${tile}_${lname}.h5)</code></small>`,
+                    "</a>",
+                    `<span class="badge badge-info badge-pill">${ldata['date']}</span>`,
+                    '</li>');
+                $downloadModalLinks.append(links);
+
+            });
+
+            $downloadModal.modal();
+        });
+
+
     });
 });
 
