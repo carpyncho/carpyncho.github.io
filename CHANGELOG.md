64969bf631bc996e77799c2bb648f93266485f56
Juan B Cabral <jbc.develop@gmail.com>
2020-04-26 04:51:09 -0300

    updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+## ver. 2020-04-26
+
+- Python client logo and link.
+- Other catalogs implementation.
+- Catalogs help link.
+
 ## ver. 2020-04-21
 
 - Added info about file sizes.
@@ -11,6 +17,7 @@
 - Upload light-curves y features for the tiles `b206`, `b214`, `b216`,
  `b220`, `b228`, `b234`, `b247`, `b248`, `b261`, `b262`, `b263`, `b264`,
  `b277`, `b278`, `b356`, `b360` and `b396`
+- 12 millions of lightcurves and 11.5 features.
 
 
 ## ver. 2020-04-13
--- static/carpyncho.js
+++ static/carpyncho.js
@@ -57,44 +57,85 @@ $.getJSON({
     cache: false
 }).then(json => {
     $.each(json, function(tile, tdata){
-        var $tiletd = $('td#tile-' + tile);
-
-        $tiletd.addClass("table-primary tile-active");
-
-        $tiletd.click(function(evt){
-            $downloadModal.find("#downloadModalTileNameTitle").text(tile);
-            $downloadModalLinks.empty();
-
-            $.each(tdata, function(lname, ldata){
-
-                var url = `https://drive.google.com/open?id=${ldata["driveid"]}`
-                var size = humanFileSize(ldata['size'], false);
-
-                var links = " ".concat(
-                    '<li class="list-group-item ">',
-                        "<div class='d-flex justify-content-between align-items-center'>",
-                            `<a target="_new" href="${url}" class="">`,
-                                `<i class="fab fa-google-drive"></i> ${ldata["hname"]}`,
-                            "</a>",
-                            `<span class="badge badge-info badge-pill">${size}</span>`,
-                            `<span class="badge badge-secondary badge-pill">Updated ${ldata['date']}</span>`,
-                        "</div>",
-                        "<div class='d-flex justify-content-between align-items-center'>",
-                            `<small><b>Format:</b> ${ldata["format"]} (<code>${ldata['extension']}</code>)</small>`,
-                            `<small>MD5: <code>${ldata['md5sum']}</code></small>`,
-                        "</div>",
-                    '</li>');
-
-                $downloadModalLinks.append(links);
-            });
-            $downloadModal.modal();
-        });
-
-
-
-
+        if(tile == "others") {
+            manageOthersCatalog(tile, tdata);
+        } else {
+            manageTileCatalog(tile, tdata);
+        }
     });
 });
+
+var manageOthersCatalog = function(tile, tdata){
+    var $otherUl = $("#other-catalogs");
+    $.each(tdata, function(lname, ldata){
+        var ocid = `othercat-${lname}`;
+        var link = `<li id="${ocid}"><a href="#">${ldata["hname"]}</a></li>`;
+        $otherUl.append(link);
+
+        var title = `Catalog ${ldata["hname"]}`;
+        $otherUl.find(`#${ocid} > a`).click(function(evt){
+
+            $downloadModal.find("#downloadModalTileNameTitle").text(title);
+            $downloadModalLinks.empty();
+
+            var url = `https://drive.google.com/open?id=${ldata["driveid"]}`
+            var size = humanFileSize(ldata['size'], false);
+
+            var links = " ".concat(
+                '<li class="list-group-item ">',
+                    "<div class='d-flex justify-content-between align-items-center'>",
+                        `<a target="_new" href="${url}" class="">`,
+                            `<i class="fab fa-google-drive"></i> ${ldata["hname"]}`,
+                        "</a>",
+                        `<span class="badge badge-info badge-pill">${size}</span>`,
+                        `<span class="badge badge-secondary badge-pill">Updated ${ldata['date']}</span>`,
+                    "</div>",
+                    "<div class='d-flex justify-content-between align-items-center'>",
+                        `<small><b>Format:</b> ${ldata["format"]} (<code>${ldata['extension']}</code>)</small>`,
+                        `<small>MD5: <code>${ldata['md5sum']}</code></small>`,
+                    "</div>",
+                '</li>');
+            $downloadModalLinks.append(links);
+            $downloadModal.modal();
+        })
+    });
+
+}
+
+var manageTileCatalog = function(tile, tdata){
+    var $clickeable = $('td#tile-' + tile);
+    var title = `Tile ${tile}`;
+    $clickeable.addClass("table-primary tile-active");
+
+    $clickeable.click(function(evt){
+        $downloadModal.find("#downloadModalTileNameTitle").text(title);
+        $downloadModalLinks.empty();
+
+        $.each(tdata, function(lname, ldata){
+
+            var url = `https://drive.google.com/open?id=${ldata["driveid"]}`
+            var size = humanFileSize(ldata['size'], false);
+
+            var links = " ".concat(
+                '<li class="list-group-item ">',
+                    "<div class='d-flex justify-content-between align-items-center'>",
+                        `<a target="_new" href="${url}" class="">`,
+                            `<i class="fab fa-google-drive"></i> ${ldata["hname"]}`,
+                        "</a>",
+                        `<span class="badge badge-info badge-pill">${size}</span>`,
+                        `<span class="badge badge-secondary badge-pill">Updated ${ldata['date']}</span>`,
+                    "</div>",
+                    "<div class='d-flex justify-content-between align-items-center'>",
+                        `<small><b>Format:</b> ${ldata["format"]} (<code>${ldata['extension']}</code>)</small>`,
+                        `<small>MD5: <code>${ldata['md5sum']}</code></small>`,
+                    "</div>",
+                '</li>');
+
+            $downloadModalLinks.append(links);
+        });
+        $downloadModal.modal();
+    });
+}
 
 
 });
